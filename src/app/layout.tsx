79f53233d9cb6a57f0398b82f9c6e0bc9901c6c3
jian <fengjian1114@gmail.com>
2025-07-30 07:52:43 -0400

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M87JH2E7DL"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M87JH2E7DL');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
